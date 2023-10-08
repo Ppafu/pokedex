8465d09d203344274ea617f87d57ba5d8001cdd1
Ppafu <inessa.evs@gmail.com>
2023-10-08 15:07:37 +0300

    config

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
 export const API_URL = `https://pokeapi.co/api/v2/`;
-export const RES_PER_PAGE = window.innerWidth > 979 ? 16 : 18;
+export const RES_PER_PAGE = window.innerWidth > 979 ? 32 : 24;
 export const TIMEOUT_SEC = 25;
 export const NUMBER_OF_POKEMONS = 1010; //1st gen 151 // 1010 all standart pokemons on 09.2023
